da3adfe2afa2655e5f8676c45896e716c267d4e7
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-11-05 02:34:50 +0600

    Weather App

---
+++ src/components/Titles.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Titles extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Weather Finder</h1>
+                <p>Find out temparature, conditions and more...</p>
+            </div>
+        )
+    }
+}
